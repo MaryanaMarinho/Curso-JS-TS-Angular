c04340bba704b4691e6bd9a14202335e70e68223
Maryana <maryanalirius@hotmail.com>
2018-10-07 19:36:36 -0300

    Modulo javaScript

---
+++ es6/arrow_functions.js
@@ -0,0 +1,12 @@
+var dobroDoValor = function(numero) {
+    return numero * 2;
+}
+
+console.log(dobroDoValor(7));
+
+
+//arrow_function
+var dobroDoValor = numero => numero * 2;
+
+
+console.log(dobroDoValor(3));
